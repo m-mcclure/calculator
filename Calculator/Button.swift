1f92fa1b71a9db0e0230e50e400315fd39a0e221
mcclure15 <m001010111@gmail.com>
2015-07-04 18:50:43 -0700

    layout work

---
+++ Calculator/Button.swift
@@ -0,0 +1,17 @@
+//
+//  Button.swift
+//  Calculator
+//
+//  Created by Matthew McClure on 7/1/15.
+//  Copyright (c) 2015 Matthew McClure. All rights reserved.
+//
+
+import UIKit
+
+class Button: UIButton {
+  required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+//    self.layer.cornerRadius = 25
+//    println(self.bounds.size.width)
+  }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var display: UILabel!
+  
+  var userIsTyping : Bool = false
+  
+  @IBAction func digitPressed(sender: UIButton) {
+    let digit = sender.currentTitle!
+    if userIsTyping {
+      display.text = display.text! + digit
+    } else {
+      display.text = digit
+      userIsTyping = true
+    }
+  }
+  
   override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
